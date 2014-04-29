25f0d02f7cba7d064c7cd398207e53daf2777113
Ben Christensen <benjchristensen@netflix.com>
2014-04-28 23:07:35 -0700

    Ignore /bin

--- reactive-lab-edge/bin/jsx/endpoints/observable_java.js
+++
@@ -1,7 +0,0 @@
-/*
- * Prove we can return Java types.
- * TODO Explore interaction between RxJava and RxJS 
- */
-function execute(parameters) {
-	return Java.type("rx.Observable").from("one", "two", "three")
-}
--- reactive-lab-edge/bin/jsx/endpoints/observable_js.js
+++
@@ -1,7 +0,0 @@
-/*
- * Prove we can return Java types.
- * TODO Explore interaction between RxJava and RxJS 
- */
-function execute(parameters) {
-	return Rx.Observable.fromArray(["one", "two", "three"])
-}
